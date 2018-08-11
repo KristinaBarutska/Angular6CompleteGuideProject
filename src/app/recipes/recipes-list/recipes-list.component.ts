9d1d958f5fb537e94dba14158e2518d8a8bc13c1
KristinaBarutska <kristina.barutska@abv.bg>
2018-08-11 08:46:10 +0300

    Added project skeleton

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+    'Test recipe 1',
+    'Test 1',
+    'https://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg'),
+    new Recipe(
+      'Test recipe 2',
+      'Test 2',
+      'https://img.bestrecipes.com.au/rZFo7F8i/h300-w400-cscale-1495077669/br-api/asset/20771/super-easy-pizza-dough-recipe.jpg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
